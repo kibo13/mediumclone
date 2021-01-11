2dc320a5dc6a1646751a2baf41826a1bed2daa58
Boris Kim <kimboris1310@gmail.com>
2021-01-11 22:08:59 +0500

    localStorage: saving token access after registration

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import authApi from '@/api/auth'
+import {setItem} from '@/helpers/persistanceStorage'
 
 const state = {
   isSubmitting: false,
@@ -33,6 +34,7 @@ const actions = {
         .register(credentials)
         .then(response => {
           context.commit('registerSuccess', response.data.user)
+          setItem('accessToken', response.data.user.token)
           resolve(response.data.user)
         })
         .catch(result => {
