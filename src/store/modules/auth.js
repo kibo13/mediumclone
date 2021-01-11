0356b25273e37daf6cad40eea5ed1e9ca8bc4cd7
Boris Kim <kimboris1310@gmail.com>
2021-01-11 23:06:43 +0500

    component Login was created, using vuex-mapState in the computed property

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -11,7 +11,11 @@ const state = {
 export const mutationTypes = {
   registerStart: '[auth] registerStart',
   registerSuccess: '[auth] registerSuccess',
-  registerFailure: '[auth] registerFailure'
+  registerFailure: '[auth] registerFailure',
+
+  loginStart: '[auth] loginStart',
+  loginSuccess: '[auth] loginSuccess',
+  loginFailure: '[auth] loginFailure'
 }
 
 const mutations = {
@@ -29,11 +33,28 @@ const mutations = {
   [mutationTypes.registerFailure](state, payload) {
     state.isSubmitting = false
     state.validationErrors = payload
+  },
+
+  [mutationTypes.loginStart](state) {
+    state.isSubmitting = true
+    state.validationErrors = null
+  },
+
+  [mutationTypes.loginSuccess](state, payload) {
+    state.isSubmitting = false
+    state.currentUser = payload
+    state.isLoggedIn = true
+  },
+
+  [mutationTypes.loginFailure](state, payload) {
+    state.isSubmitting = false
+    state.validationErrors = payload
   }
 }
 
 export const actionTypes = {
-  register: '[auth] register'
+  register: '[auth] register',
+  login: '[auth] login'
 }
 
 const actions = {
@@ -50,6 +71,25 @@ const actions = {
         .catch(result => {
           context.commit(
             mutationTypes.registerFailure,
+            result.response.data.errors
+          )
+        })
+    })
+  },
+
+  [actionTypes.login](context, credentials) {
+    context.commit(mutationTypes.loginStart)
+    return new Promise(resolve => {
+      authApi
+        .login(credentials)
+        .then(response => {
+          context.commit(mutationTypes.loginSuccess, response.data.user)
+          setItem('accessToken', response.data.user.token)
+          resolve(response.data.user)
+        })
+        .catch(result => {
+          context.commit(
+            mutationTypes.loginFailure,
             result.response.data.errors
           )
         })
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,23 +3,15 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign up</h1>
+          <h1 class="text-xs-center">Sign in</h1>
           <p class="text-xs-center">
-            <router-link :to="{name: 'login'}">Have an account?</router-link>
+            <router-link :to="{name: 'register'}">Need an account?</router-link>
           </p>
           <mcv-validation-errors
             v-if="validationErrors"
             :validation-errors="validationErrors"
           />
           <form @submit.prevent="onSubmit">
-            <fieldset class="form-group">
-              <input
-                type="text"
-                class="form-control form-control-lg"
-                placeholder="Username"
-                v-model="username"
-              />
-            </fieldset>
             <fieldset class="form-group">
               <input
                 type="text"
@@ -40,7 +32,7 @@
               class="btn btn-lg btn-primary pull-xs-right"
               :disabled="isSubmitting"
             >
-              Sign up
+              Sign in
             </button>
           </form>
         </div>
@@ -55,15 +47,14 @@ import McvValidationErrors from '@/components/ValidationErrors'
 import {actionTypes} from '@/store/modules/auth'
 
 export default {
-  name: 'McvRegister',
+  name: 'McvLogin',
   components: {
     McvValidationErrors
   },
   data() {
     return {
       email: '',
-      password: '',
-      username: ''
+      password: ''
     }
   },
   computed: {
@@ -75,9 +66,8 @@ export default {
   methods: {
     onSubmit() {
       this.$store
-        .dispatch(actionTypes.register, {
+        .dispatch(actionTypes.login, {
           email: this.email,
-          username: this.username,
           password: this.password
         })
         .then(() => {
