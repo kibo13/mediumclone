43f35cd8002fbdf78d92feb2bbae4822b486d454
unknown <ARM-1@ARM1.cenki.ru>
2021-01-12 08:05:49 +0100

    markup added for Feed component

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,19 +1,70 @@
 <template>
   <div>
-    Feed is here
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happened</div>
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" alt="" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username}
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">
+            ADD TO FAVORITES
+          </div>
+        </div>
+        <router-link
+          :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import {actionTypes} from '@/store/modules/feed'
 
 export default {
   name: 'McvFeed',
+
   props: {
     apiUrl: {
       type: String,
       required: true
     }
+  },
+
+  computed: {
+    ...mapState({
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
+    })
   },
 
   mounted() {
