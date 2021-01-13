a2db17afbc6aa53bcc9d8ad58f1de46977f405f5
unknown <ARM-1@ARM1.cenki.ru>
2021-01-13 13:56:04 +0100

    component FeedToggler was created

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -4,6 +4,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
+          <mcv-feed-toggler />
           <mcv-feed :api-url="apiUrl" />
         </div>
         <div class="col-md-3">
@@ -18,13 +19,15 @@
 import McvFeed from '@/components/Feed'
 import McvPopularTags from '@/components/PopularTags'
 import McvBanner from '@/components/Banner'
+import McvFeedToggler from '@/components/FeedToggler'
 
 export default {
   name: 'McvGlobalFeed',
   components: {
     McvFeed,
     McvPopularTags,
-    McvBanner
+    McvBanner,
+    McvFeedToggler
   },
   data() {
     return {
