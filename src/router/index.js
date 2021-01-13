1105546b1326b7ed47f37e64ce3b9ab16e52dadf
unknown <ARM-1@ARM1.cenki.ru>
2021-01-13 13:33:31 +0100

    views TagFeed and YourFeed were created

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import GlobalFeed from '@/views/GlobalFeed'
+import YourFeed from '@/views/YourFeed'
+import TagFeed from '@/views/TagFeed'
 import Register from '@/views/Register'
 import Login from '@/views/Login'
 
@@ -26,12 +28,12 @@ const routes = [
   {
     path: '/feed',
     name: 'yourFeed',
-    component: GlobalFeed
+    component: YourFeed
   },
   {
     path: '/tags/:slug',
     name: 'tag',
-    component: GlobalFeed
+    component: TagFeed
   },
   {
     path: '/articles/new',
